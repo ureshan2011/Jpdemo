13c066b56ae1e32370297b89e1b058d2f0a89a14
Nadeera_zincat <nadeera.liyanage@dialog.lk>
2021-04-02 17:54:42 +0530

    Initial commit

---
+++ src/main/java/com/example/Jpdemo/JpdemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.Jpdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpdemoApplication.class, args);
+		System.out.println("try");
+	}
+
+}
---
+++ src/test/java/com/example/Jpdemo/JpdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Jpdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
