74b87e757cd18604d8edd1c24519268b492b51a7
Yasas Sri Wickramasinghe <yasas@Yasass-MacBook-Air.local>
2021-04-03 10:36:14 +0530

    adding component scan

--- src/main/java/com/example/Jpdemo/JpdemoApplication.java
+++ src/main/java/com/example/Jpdemo/JpdemoApplication.java
@@ -1,14 +1,14 @@
 package com.example.Jpdemo;
 
+import com.example.Controler.UserController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.example"})
 public class JpdemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpdemoApplication.class, args);
-
 	}
 
 }
